afb0f7b36fa4bc63c5b16bae8f3a441600729c6f
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-05-15 16:18:41 -0300

    First attempt

--- src/custom-code/user-input-currency-options.js
+++ src/custom-code/user-input-currency-options.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import currencyNames from './data-currency';
 
 const CurrencyOptions = function(){
+    const Names = currencyNames.map( (name) => <option>{name}</option>);
+    //Parei aqui
     return (
         <select>
-            <option>Cur1</option>
-            <option>Cur2</option>
-            <option>Cur3</option>
-            <option>Cur4</option>
-            <option>Cur5</option>
+            {/*<Names />*/}
+            <option>CUR1</option>
+            <option>CUR2</option> 
+            <option>CUR3</option> 
+            <option>CUR4</option> 
+            <option>CUR5</option> 
+            
         </select>
     )
 }
