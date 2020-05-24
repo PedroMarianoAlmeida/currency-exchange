adb6c533f2521c9618743870968deed3e792e8b3
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-05-23 21:18:42 -0300

    Finishing the new currency form

--- src/custom-code/new-currency.js
+++ src/custom-code/new-currency.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 class NewCurrency extends React.Component {
+    
+
+    handleSubmit(e){
+        e.preventDefault();
+    }
+    
     render(){
         return(
-            <h1 className='mt-5'>New currency</h1>
+            <div className='container'>
+                <div className="row">
+                    <div className='col-11'>
+                        <h1 className='mt-5'>New currency</h1>
+                        <form onSubmit={this.handleSubmit}>
+                            <label className='d-block'>Currency required</label>
+                            <input type='text' className='form-control w-50 d-block mb-3' placeholder='Currency' required/>
+                            <br />
+                            <label className='d-block'> Your Email (opcional)</label>
+                            <input type='email' className='form-control w-50 d-block mb-3' placeholder='E-mail' />
+                            <br />
+                            <button type='submit' class='btn btn-dark'>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
