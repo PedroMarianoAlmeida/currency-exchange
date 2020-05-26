9333c32287ccbc71b382dc43a2eb9f8afde33149
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-05-26 08:56:21 -0300

    Cells input show onMouseOver

--- src/custom-code/main-content-currency-value.js
+++ src/custom-code/main-content-currency-value.js
@@ -5,6 +5,7 @@ class CurrencyValueColumn extends React.Component {
         super(props);
         this.state = {
             number: 0,
+            visible: false
         }
         this.handleChange = this.handleChange.bind(this);
     }
@@ -24,11 +25,26 @@ class CurrencyValueColumn extends React.Component {
         console.log(this.state.number)
     }
 
+    handleMouseOver(){
+        this.setState({
+            visible: true
+        })
+    }
+
+    handleMouseOut(){
+        this.setState({
+            visible: false
+        })
+    }
+
     render(){
+        const show = this.state.visible? 1 : 0;       
         return(
             <div>
                 <input type='number' min='0' step='0.01' className='text-center d-block'
-                onChange={this.handleChange}></input>  
+                onChange={this.handleChange} onMouseOver={this.handleMouseOver.bind(this)}
+                onMouseOut={this.handleMouseOut.bind(this)} style={{opacity:show}}
+                ></input>  
             </div>
         )
     }
--- src/custom-code/main-content.js
+++ src/custom-code/main-content.js
@@ -65,42 +65,16 @@ class MainContent extends React.Component {
     render(){
         const cellsTableCurrency = this.state.columnsCurrency.map ( (currency, index) => {
             return(
-                <tr>
+                <tr key={currency}>
                     <th scope='row'>{currency}</th>
                     <td className='text-center'> {(this.state.columnsRate[index] * this.state.currentNumber).toFixed(2)}</td>
-                    <CurrencyValueColumn updateUserInputNumber={this.updateUserInputNumber} currencyRate={this.state.columnsRate[index]}/>
+                    <td><CurrencyValueColumn updateUserInputNumber={this.updateUserInputNumber} currencyRate={this.state.columnsRate[index]}/></td>                  
                 </tr>
             )
         });
         
         return(
             <div>
-                <div className='d-none'> {/*Descktop version*/}
-                    <table className="table mt-5 w-75">
-                    <thead>
-                            <CurrencyColumns currency={this.state.columnsCurrency}/>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <th scope="row"> 
-                                <div>
-                                    <CurrencyValue updateUserInputNumber={this.updateUserInputNumber} currentNumber={this.state.currentNumber}/>
-                                </div>
-                                <div className='d-flex justify-content-center'>
-                                    <UserInputCurrency updateUserInputCurrency={this.updateCurrentCurrency}/>                            
-                                </div>
-                            </th>
-                            <td> <CurrencyValueColumn number={this.state.columnsRate[0] * this.state.currentNumber} updateUserInputNumber={this.updateUserInputNumber} rate={this.state.columnsRate[0]}/>  </td>                   
-                            <td>{(this.state.columnsRate[1] * this.state.currentNumber).toFixed(2)}</td> 
-                            <td>{(this.state.columnsRate[2] * this.state.currentNumber).toFixed(2)}</td> 
-                            <td>{(this.state.columnsRate[3] * this.state.currentNumber).toFixed(2)}</td> 
-                            <td>{(this.state.columnsRate[4] * this.state.currentNumber).toFixed(2)}</td> 
-                        </tr>
-                    </tbody>
-                    </table>
-                </div>
-
                 <div className="flex-grow-1"> {/*Mobile version*/}
                     <table className="table mt-5 w-75">
                         <thead>
